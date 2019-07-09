d1ecf57bdbeef3edfe1a0473f3c5a33023ad3638
shubham <shubhamrwt02@gmail.com>
2019-07-10 00:46:22 +0530

    lect

--- src/com/company/Lec9/Streams.java
+++ src/com/company/Lec9/Streams.java
@@ -3,7 +3,7 @@ package com.company.Lec9;
 public class Streams {
 
     public static void main(String[] args) {
-        System.out.println(replacepi("","pppipppi"));
+        System.out.println(skiphimodified("","hihithi"));
     }
 
 
@@ -16,7 +16,7 @@ public class Streams {
         char ch = unprocessed.charAt(0);
 
         if (ch == 'p' && unprocessed.charAt(1) == 'i') {
-            return replacepi(processed + "3.14", unprocessed.substring(2));
+            return replacepi(processed, unprocessed.substring(2));
         } else {
             return replacepi(processed + ch, unprocessed.substring(1));
         }
@@ -30,10 +30,39 @@ public class Streams {
 
         char ch = unprocessed.charAt(0);
 
-        if (ch == 'p' && unprocessed.charAt(1) == 'i') {
-            return replacepi(processed , unprocessed.substring(2));
+        if (ch == 'h' && unprocessed.charAt(1) == 'i') {
+            return skiphi(processed , unprocessed.substring(2));
         } else {
-            return replacepi(processed + ch, unprocessed.substring(1));
+            return skiphi(processed + ch, unprocessed.substring(1));
         }
+    }
+
+    public static String skiphimodified(String processed ,String unprocessed) {
+
+        if (unprocessed.length() < 2) {
+            return processed + unprocessed;
+        }
+
+        char ch = unprocessed.charAt(0);
+
+        if (ch == 'h' && unprocessed.charAt(1) == 'i') {
+            if (unprocessed.length() > 2) {
+
+                if (unprocessed.charAt(2) == 't') {
+                    return skiphimodified(processed + unprocessed.substring(0, 3), unprocessed.substring(3));
+                }
+                else {
+                    return skiphimodified(processed, unprocessed.substring(2));
+                }
+            }
+            else {
+                return skiphimodified(processed, unprocessed.substring(2));
+            }
+        }
+               else {
+            return skiphimodified(processed + ch, unprocessed.substring(1));
+        }
+
+
     }
 }
---
+++ src/com/company/Lec9/mazepath.java
@@ -0,0 +1,69 @@
+package com.company.Lec9;
+
+import java.util.ArrayList;
+
+public class mazepath {
+
+
+    public static void main(String[] args) {
+
+        ArrayList list=new ArrayList();
+//        listmazepath("",3,3,list);
+        System.out.println(list2mazepath("",3,3));
+//        System.out.println(list);
+    }
+
+    public static void listmazepath(String processed,int row,int col,ArrayList list){
+
+        if(row==1&&col==1){
+            list.add(processed);
+            return;
+        }
+
+        if(row>1){
+            listmazepath(processed+"V",row-1,col,list);
+        }
+
+        if(col>1){
+            listmazepath(processed+"H",row,col-1,list);
+        }
+
+    }
+
+    public static void mazepath(String processed,int row,int col) {
+
+        if (row == 1 && col == 1) {
+            System.out.println(processed);
+            return;
+        }
+
+        if (row > 1) {
+            mazepath(processed + "V", row - 1, col);
+        }
+
+        if (col > 1) {
+            mazepath(processed + "H", row, col - 1);
+        }
+    }
+
+        public static ArrayList list2mazepath(String processed,int row,int col){
+
+            if(row==1&&col==1){
+                ArrayList list =new ArrayList();
+                list.add(processed);
+                return list;
+            }
+             ArrayList list =new ArrayList();
+
+            if(row>1){
+               list.addAll(list2mazepath(processed+"V",row-1,col));
+            }
+
+            if(col>1){
+                list.addAll(list2mazepath(processed+"H",row,col-1));
+            }
+            return list;
+        }
+
+    }
+
